00a953a1ab21df71b257ecd295ef7c3593dc64f3
minkzaw <minkhantzaw.personal@gmail.com>
2024-03-19 14:48:12 +0630

    Add Checkport object to use in the API.

---
+++ portchecker.py
@@ -0,0 +1,28 @@
+from socket import socket, AF_INET, SOCK_STREAM, error as SocketError
+
+class Checkport:
+    def __init__(self, ip=None, port=None):
+        self.ip = ip
+        self.port = port
+
+    def portChecker(self):
+        session = None  # Initialize session outside the try block
+        try:
+            if self.ip is not None and self.port is not None:
+                session = socket(AF_INET, SOCK_STREAM)
+                session.settimeout(2)
+                session.connect((self.ip, self.port))
+                return f"The {self.port} on {self.ip} is open."
+            else:
+                return "Please specify IP and Port."
+                
+        except SocketError as error:
+            return f"The {self.port} on {self.ip} is not reachable."
+        
+        finally:
+            if session is not None:
+                session.close()  # Close session if it's not None
+
+# Instantiating the class and calling the method correctly
+myTesting = Checkport(ip="8.8.8.8",port=53)
+print(myTesting.portChecker())  # No additional arguments needed here
